619b9e299b930e298849c8f5bc6b471184d488c0
vedh-23 <134149515+vedh-23@users.noreply.github.com>
2023-08-17 17:35:16 +0530

    Initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,24 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY BIRTHDAY Dr Puja!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">HAPPY BIRTHDAY Puja Didi!</h3>
+      <p>Dear Di,</p>
+      <p>
+        Happy birthday!! I hope your day is filled with lots of love and
+        laughter! May all of your birthday wishes come true.You have a very
+        special place in my life.You are my inspiration,devotion,motivation. May
+        Mahadev bless you on your special day.
+      </p>
+      <p className="name">Vedh</p>
+    </div>
+  </div>
+);
